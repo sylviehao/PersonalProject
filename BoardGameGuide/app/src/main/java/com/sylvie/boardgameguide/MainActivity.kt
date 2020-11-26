fdbc2928fd3139883f3836749d751c3cbe93692d
sylviehao <sylviasylvie199485@gmail.com>
2020-11-26 23:18:05 +0800

    Add toolbar and fab to HomeFragment

--- BoardGameGuide/app/src/main/java/com/sylvie/boardgameguide/MainActivity.kt
+++ BoardGameGuide/app/src/main/java/com/sylvie/boardgameguide/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.sylvie.boardgameguide
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
+import com.sylvie.boardgameguide.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.TRANSPARENT
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
     }
 }
--- BoardGameGuide/app/src/main/java/com/sylvie/boardgameguide/home/HomeFragment.kt
+++ BoardGameGuide/app/src/main/java/com/sylvie/boardgameguide/home/HomeFragment.kt
@@ -1,17 +1,38 @@
 package com.sylvie.boardgameguide.home
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.madapps.liquid.LiquidRefreshLayout
+import com.sylvie.boardgameguide.databinding.HomeFragmentBinding
+import kotlinx.android.synthetic.main.home_fragment.*
 
 class HomeFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = HomeFragmentBinding.inflate(inflater, container,false)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.refreshLayout.setOnRefreshListener(object : LiquidRefreshLayout.OnRefreshListener {
+            override fun completeRefresh() {
+            }
+            override fun refreshing() {
+//                adapter.notifyDataSetChanged()
+                object : CountDownTimer(2000, 1000) {
+
+                    override fun onFinish() {
+                        binding.refreshLayout.finishRefreshing()
+                    }
+                    override fun onTick(millisUntilFinished: Long) {
+                    }
+                }.start()
+            }
+        })
+
+
+
+        return binding.root
     }
 }
